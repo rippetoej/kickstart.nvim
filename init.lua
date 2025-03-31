8acf8426c0e4a2cd7c8740a9b10ee9b525e03e7b
John Rippetoe <rippetoej@gmail.com>
2025-03-30 23:42:48 -0400

    Added cscope plugin

--- init.lua
+++ init.lua
@@ -961,7 +961,56 @@ require('lazy').setup({
       vim.keymap.set('n', '<C-Space>', nvim_tmux_nav.NvimTmuxNavigateNext)
     end,
   },
+  {
+    'dhananjaylatkar/cscope_maps.nvim',
+    dependencies = {
+      'nvim-telescope/telescope.nvim', -- optional [for picker="telescope"]
+      'ibhagwan/fzf-lua', -- optional [for picker="fzf-lua"]
+      'echasnovski/mini.pick', -- optional [for picker="mini-pick"]
+      'folke/snacks.nvim', -- optional [for picker="snacks"]
+    },
+    opts = {
+      -- maps related defaults
+      disable_maps = false, -- "true" disables default keymaps
+      skip_input_prompt = true, -- "true" doesn't ask for input
+      prefix = '<leader>c', -- prefix to trigger maps
 
+      -- cscope related defaults
+      cscope = {
+        -- location of cscope db file
+        db_file = './cscope.out', -- DB or table of DBs
+        -- NOTE:
+        --   when table of DBs is provided -
+        --   first DB is "primary" and others are "secondary"
+        --   primary DB is used for build and project_rooter
+        -- cscope executable
+        exec = 'gtags-cscope', -- "cscope" or "gtags-cscope"
+        -- choose your fav picker
+        picker = 'snacks', -- "quickfix", "telescope", "fzf-lua", "mini-pick" or "snacks"
+        -- size of quickfix window
+        qf_window_size = 5, -- any positive integer
+        -- position of quickfix window
+        qf_window_pos = 'bottom', -- "bottom", "right", "left" or "top"
+        -- "true" does not open picker for single result, just JUMP
+        skip_picker_for_single_result = false, -- "false" or "true"
+        -- custom script can be used for db build
+        db_build_cmd = { script = 'default', args = { '-bqkv' } },
+        -- statusline indicator, default is cscope executable
+        statusline_indicator = nil,
+        -- try to locate db_file in parent dir(s)
+        project_rooter = {
+          enable = false, -- "true" or "false"
+          -- change cwd to where db_file is located
+          change_cwd = false, -- "true" or "false"
+        },
+      },
+
+      -- stack view defaults
+      stack_view = {
+        tree_hl = true, -- toggle tree highlighting
+      },
+    },
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
